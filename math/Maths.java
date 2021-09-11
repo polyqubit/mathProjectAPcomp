f2cbb7f49d8d3b10ff874008301b5e01f69b9986
polyqubit <polyumbrial@gmail.com>
2021-09-10 20:53:25 -0700

    new methods

--- math/Maths.java
+++ math/Maths.java
@@ -8,29 +8,68 @@ public final class Maths {
 	}
 	
 	//floor, takes double
-	public static int floorD(double inpu) {
-		return (int) inpu;
+	public static int floorD(double inp) {
+		return (int) inp;
 	}
 	
 	//ceiling command, takes double
-	public static int ceilD(double inpu) {
-		int benchmark = (int) inpu;
-		if(inpu>benchmark){return (int) inpu+1;}
-		else if(inpu==benchmark){return (int) inpu;}
+	public static int ceilD(double inp) {
+		int benchmark = (int) inp;
+		if(inp>benchmark){return (int) inp+1;}
+		else if(inp==benchmark){return (int) inp;}
 		else{return 0;}
 	}
 	
 	//round command, takes double
-	public static int round(double inpu) {
-		double benchmark = (int) inpu+0.5;
-		if(inpu<benchmark){return (int) inpu;}
-		else if(inpu>benchmark){return (int) inpu+1;}
-		else{return (int) inpu+1;}
+	public static int round(double inp) {
+		double benchmark = (int) inp+0.5;
+		if(inp<benchmark){return (int) inp;}
+		else if(inp>benchmark){return (int) inp+1;}
+		else{return (int) inp+1;}
 	}
 	
 	//absolute value command, takes double
-	public static double abs(double inpu) {
-		if(inpu>0){return inpu;}
-		else{return inpu*(-1);}
+	public static double abs(double inp) {
+		if(inp>0){return inp;}
+		else{return inp*(-1);}
+	}
+	
+	//maximum command, takes unlimited doubles
+	public static double max(double ... inp) {
+		double maxinp = inp[0];
+		for(int i=1;i<inp.length;i++) {
+			if (inp[i]>maxinp) {maxinp=inp[i];}
+		}
+		return maxinp;
+	}
+	
+	//minimum command, takes unlimited doubles
+	public static double min(double ... inp) {
+		double mininp = inp[0];
+		for(int i=1;i<inp.length;i++) {
+			if (inp[i]<mininp) {mininp=inp[i];}
+		}
+		return mininp;
+	}
+	
+	//power command, takes a double base and an int exponent
+	public static double pow(double base, int exp) {
+		if(exp<=0) {return 1;}
+		int c = 1;
+		double d = base;
+		while(c<exp) {d=d*base;c++;}
+		return d;
+	}
+	
+	//root command, takes an int base and an int root
+	//note: not my code, couldn't figure out a way to do this
+	public static double sqrt(int base) {
+		double a = 0;
+		double sqt = base/2;
+		while((a-sqt)!=0){
+			a = sqt;
+			sqt = (a+(base/a))/2;
+		}
+		return sqt;
 	}
 }
--- math/starter.java
+++ math/starter.java
@@ -9,5 +9,9 @@ public class starter {
 		System.out.println(Maths.ceilD(test));
 		System.out.println(Maths.round(900000.121));
 		System.out.println(Maths.abs(-6));
+		System.out.println(Maths.max(1,2,3,4,9,0.07));
+		System.out.println(Maths.min(-100,200,300,400,-9.0));
+		System.out.println(Maths.pow(-9,3));
+		System.out.println(Maths.sqrt(11));
 	}
 }
