9055be875f85ea0f1d6d3c99ded85ed4bab0b942
polyqubit <polyumbrial@gmail.com>
2021-09-14 23:06:39 -0700

    final(probably)

--- math/starter.java
+++ math/starter.java
@@ -21,28 +21,37 @@ public class starter {
 		
 		test code 
 		**/
+		System.out.print("baseArray[ ");
 		ArrayList<Double> randoms = new ArrayList<Double>(1000);
-		for(int i=0;i<randoms.size();i++) {
-			randoms.set(i,Maths.rand(100));
+		for(int i=0;i<1000;i++) {
+			randoms.add(Maths.rand(100));
 			Canvas.pause(1);
+			if(i<999){System.out.print(randoms.get(i)+", ");}
+			else{System.out.print(randoms.get(i));break;}
 		}
+		System.out.print(" ]\n\n\n");
 		
-		System.out.println("Base array:\n"+Arrays.toString(randoms.toArray()));
+		//System.out.println("Base array:\n"+Arrays.toString(randoms.toArray()));
 		
+		System.out.print("powArray[ ");
 		ArrayList<Double> pows = new ArrayList<Double>(1000);
-		for(int i=0;i<pows.size();i++) {
-			pows.set(i,Maths.pow(randoms.get(i),2));
+		for(int i=0;i<1000;i++) {
+			pows.add(Maths.pow(randoms.get(i),2));
 			Canvas.pause(1);
+			if(i<999){System.out.print(pows.get(i)+", ");}
+			else{System.out.print(pows.get(i));break;}
 		}
+		System.out.print(" ]\n\n\n");
 
-		System.out.println("Squared array:\n"+Arrays.toString(randoms.toArray()));
-		
+		System.out.print("sqrtArray[ ");
 		ArrayList<Double> sqrts = new ArrayList<Double>(1000);
-		for(int i=0;i<sqrts.size();i++) {
-			sqrts.set(i,Maths.sqrt(randoms.get(i)));
+		for(int i=0;i<1000;i++) {
+			sqrts.add(Maths.sqrt(randoms.get(i)));
+			Canvas.pause(1);
+			if(i<999){System.out.print(sqrts.get(i)+", ");}
+			else{System.out.print(sqrts.get(i));break;}
 		}
-
-		System.out.println("SqrRoot array:\n"+Arrays.toString(sqrts.toArray()));
+		System.out.println(" ]");
 		
 	}
 }
