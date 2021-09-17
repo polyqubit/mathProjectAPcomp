2b14908664e8b0e4c98d0b45e06d8858f9d01b29
polyqubit <polyumbrail@gmail.com>
2021-09-17 12:24:00 -0700

    ACTUAL FINAL COMMIT

--- math/starter.java
+++ math/starter.java
@@ -28,22 +28,22 @@ public class starter {
 		int col=0;
 		Rectangle point;
 		Color pc;
-		ArrayList<Double> randoms = new ArrayList<Double>(1000);
-		for(int i=0;i<1000;i++) {
+		ArrayList<Double> randoms = new ArrayList<Double>(2000);
+		for(int i=0;i<2000;i++) {
 			randoms.add(Maths.rand(255));
 			Canvas.pause(1);
 			avg += randoms.get(i);
 			point = new Rectangle(randoms.get(i)*1.5,250,2,5);
 			col = Maths.floorD(randoms.get(i));
-			pc = new Color(col,col,100);
+			pc = new Color(col,col/2,100);
 			point.setColor(pc);
 			point.fill();
 			if (randoms.get(i)>maxinp) {maxinp=randoms.get(i);}
 			if (randoms.get(i)<mininp) {mininp=randoms.get(i);}
-			if(i<999){System.out.print(randoms.get(i)+", ");}
+			if(i<1999){System.out.print(randoms.get(i)+", ");}
 			else{System.out.print(randoms.get(i));break;}
 		}
-		System.out.print(" ]\nAverage of array:"+avg/1000+"\nMax,Min of array:"+maxinp+", "+mininp+"\n\n\n");
+		System.out.print(" ]\nAverage of array:"+avg/2000+"\nMax,Min of array:"+maxinp+", "+mininp+"\n\n\n");
 		avg=0;
 		maxinp=0;
 		mininp=0;
@@ -51,33 +51,33 @@ public class starter {
 		//System.out.println("Base array:\n"+Arrays.toString(randoms.toArray()));
 		
 		System.out.print("powArray[ ");
-		ArrayList<Double> pows = new ArrayList<Double>(1000);
-		for(int i=0;i<1000;i++) {
+		ArrayList<Double> pows = new ArrayList<Double>(2000);
+		for(int i=0;i<2000;i++) {
 			pows.add(Maths.pow(randoms.get(i),2));
 			Canvas.pause(1);
 			avg+=pows.get(i);
 			if (pows.get(i)>maxinp) {maxinp=pows.get(i);}
 			if (pows.get(i)<mininp) {mininp=pows.get(i);}
-			if(i<999){System.out.print(pows.get(i)+", ");}
+			if(i<1999){System.out.print(pows.get(i)+", ");}
 			else{System.out.print(pows.get(i));break;}
 		}
-		System.out.print(" ]\nAverage of array:"+avg/1000+"\nMax,Min of array:"+maxinp+", "+mininp+"\n\n\n");
+		System.out.print(" ]\nAverage of array:"+avg/2000+"\nMax,Min of array:"+maxinp+", "+mininp+"\n\n\n");
 		avg=0;
 		maxinp=0;
 		mininp=0;
 
 		System.out.print("sqrtArray[ ");
-		ArrayList<Double> sqrts = new ArrayList<Double>(1000);
-		for(int i=0;i<1000;i++) {
+		ArrayList<Double> sqrts = new ArrayList<Double>(2000);
+		for(int i=0;i<2000;i++) {
 			sqrts.add(Maths.sqrt(randoms.get(i)));
 			Canvas.pause(1);
 			avg+=sqrts.get(i);
 			if (sqrts.get(i)>maxinp) {maxinp=sqrts.get(i);}
 			if (sqrts.get(i)<mininp) {mininp=sqrts.get(i);}
-			if(i<999){System.out.print(sqrts.get(i)+", ");}
+			if(i<1999){System.out.print(sqrts.get(i)+", ");}
 			else{System.out.print(sqrts.get(i));break;}
 		}
-		System.out.print(" ]\nAverage of array:"+avg/1000+"\nMax,Min of array:"+maxinp+", "+mininp+"\n\n\n");
+		System.out.print(" ]\nAverage of array:"+avg/2000+"\nMax,Min of array:"+maxinp+", "+mininp+"\n\n\n");
 		
 	}
 }
