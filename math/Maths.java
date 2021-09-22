95288bdb821ddb3441604ce47f4b411668ea5ebd
polyqubit <polyumbrail@gmail.com>
2021-09-22 12:47:02 -0700

    editss

--- math/Maths.java
+++ math/Maths.java
@@ -2,8 +2,8 @@ import pkg.*;
 import java.util.ArrayList;
 import java.time.Clock;
 
-class Maths {
-	private double mathnumber;
+public class Maths {
+	double mathnumber;
 	private Clock clo;
 	
 	Maths (double inp) {
@@ -45,7 +45,7 @@ class Maths {
 	}
 	
 	//maximum command, takes unlimited doubles
-	double max(double ... inp) {
+	double max(double[] inp) {
 		double maxinp = inp[0];
 		for(int i=1;i<inp.length;i++) {
 			if (inp[i]>maxinp) {maxinp=inp[i];}
@@ -54,7 +54,7 @@ class Maths {
 	}
 	
 	//minimum command, takes unlimited doubles
-	double min(double ... inp) {
+	double min(double[] inp) {
 		double mininp = inp[0];
 		for(int i=1;i<inp.length;i++) {
 			if (inp[i]<mininp) {mininp=inp[i];}
@@ -85,7 +85,7 @@ class Maths {
 	}
 	
 	//averaging function, takes unlimited doubles
-	double avg(double ... inp) {
+	double avg(double[] inp) {
 		double sum = 0;
 		for(int i=0;i<inp.length;i++) {
 			sum += inp[i];
@@ -116,91 +116,58 @@ class Maths {
 
 
 
-public class nMath extends Maths {
+class nMath extends Maths {
 	double num = 0;
-	public nMath(inp) {
+	public nMath(double inp) {
 		num = inp;
+		super(inp);
 	}
 	public int floorD() {
-		super.floorD();
+		return(super.floorD());
 	}
 	public int ceilD() {
-		super.ceilD();
+		return(super.ceilD());
 	}
 	
 	//round command, takes double
 	public int round() {
-		super.round();
+		return(super.round());
 	}
 	
 	//absolute value command, takes double
 	public double abs() {
-		super.abs();
+		return(super.abs());
 	}
 	
 	//maximum command, takes unlimited doubles
 	public double max(double ... inp) {
-		super.max();
+		return(super.max(inp));
 	}
 	
 	//minimum command, takes unlimited doubles
 	double min(double ... inp) {
-		double mininp = inp[0];
-		for(int i=1;i<inp.length;i++) {
-			if (inp[i]<mininp) {mininp=inp[i];}
-		}
-		return mininp;
+		return(super.min(inp));
 	}
 	
 	//power command, takes a double base and an int exponent
 	double pow(int exp) {
-		if(exp<=0) {return 1;}
-		int c = 1;
-		double d = mathnumber;
-		while(c<exp) {d=d*mathnumber;c++;}
-		return d;
+		return(super.pow(exp));
 	}
 	
 	//root command, takes a double base
 	//note: not my code, couldn't figure out a way to do this
 	double sqrt() {
-		if(mathnumber<0){return 0;}
-		double a = 0;
-		double sqt = mathnumber/2;
-		while((a-sqt)!=0){
-			a = sqt;
-			sqt = (a+(mathnumber/a))/2;
-		}
-		return sqt;
+		return(super.sqrt());
 	}
 	
 	//averaging function, takes unlimited doubles
 	double avg(double ... inp) {
-		double sum = 0;
-		for(int i=0;i<inp.length;i++) {
-			sum += inp[i];
-		}
-		return sum/inp.length;
+		return(super.avg(inp));
 	}
 	
 	//random command, takes an integer upper bound
 	double rand(int limit) {
-		if(limit>100000000) {limit=100000000;}
-		double tiom = System.currentTimeMillis()*System.currentTimeMillis()*30109.14159265358979;
-		int[] list = new int[limit+1];
-		for(int i=0;i<list.length;i++){
-			list[i]=i;
-		}
-		if(limit%2==0){
-			if(tiom<0) {tiom = tiom%(limit+1)*(-1);}
-			else {tiom = tiom%(limit+1);}
-		}
-		else{
-			if(tiom<0) {tiom = tiom%(limit)*(-1);}
-			else {tiom = tiom%(limit);}
-		}
-		int toim = (int) tiom;
-		return list[toim];
+		return(super.rand(limit));
 	}
 }
 
